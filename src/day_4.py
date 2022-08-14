e37388e34c763f300fe060894675fd510c2cafb1
Achxy <74144790+Achxy@users.noreply.github.com>
2022-08-14 21:22:54 +0530

    day 4

---
+++ src/day_4.py
@@ -0,0 +1,28 @@
+from hashlib import md5
+from itertools import count
+
+from benchmark import advent_problem
+from data import day_4 as DATA
+
+
+def _from_zero_count(data, zero_count):
+    zero_prefix = "".zfill(zero_count)
+    for num in count(start=1):
+        hashed = md5(f"{data.strip()}{num}".encode()).hexdigest()
+        if hashed.startswith(zero_prefix):
+            return num
+
+
+@advent_problem
+def part_1(data=DATA):
+    return _from_zero_count(data, 5)
+
+
+@advent_problem
+def part_2(data=DATA):
+    return _from_zero_count(data, 6)
+
+
+if __name__ == "__main__":
+    part_1()
+    part_2()
