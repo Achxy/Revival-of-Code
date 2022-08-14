602f75453db7bfcbe59bd7cdcd0b2155c0e51aa8
Achxy <74144790+Achxy@users.noreply.github.com>
2022-08-14 21:35:00 +0530

    use the `initial` kwarg

--- src/day_3.py
+++ src/day_3.py
@@ -3,22 +3,21 @@ from itertools import accumulate
 from benchmark import advent_problem
 from data import day_3 as DATA
 
-ORIGIN = {0}
 MOVE_INSTRUCTION = {"^": 1j, "v": -1j, "<": 1, ">": -1}
 
 
 def _conv(instructions):
-    return set(accumulate(MOVE_INSTRUCTION[move] for move in instructions))
+    return set(accumulate((MOVE_INSTRUCTION[move] for move in instructions), initial=0))
 
 
 @advent_problem
 def part_1(data=DATA):
-    return len(ORIGIN | _conv(data))
+    return len(_conv(data))
 
 
 @advent_problem
 def part_2(data=DATA):
-    return len(ORIGIN | _conv(data[::2]) | _conv(data[1::2]))
+    return len(_conv(data[::2]) | _conv(data[1::2]))
 
 
 if __name__ == "__main__":
