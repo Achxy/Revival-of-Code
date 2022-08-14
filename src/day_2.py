2a2f52bafa3331fc236adc14f81b17d53b2f9a57
Achxy <74144790+Achxy@users.noreply.github.com>
2022-08-14 21:28:15 +0530

    use the fancier `<<`

--- src/day_2.py
+++ src/day_2.py
@@ -10,12 +10,12 @@ BOXES = [[int(x) for x in line.split("x")] for line in DATA.strip().splitlines()
 
 @advent_problem
 def part_1(data=BOXES) -> int:
-    return sum(2 * sum(k := tuple(map(prod, combinations(d, 2)))) + min(k) for d in data)
+    return sum((sum(k := tuple(map(prod, combinations(d, 2)))) << 1) + min(k) for d in data)
 
 
 @advent_problem
 def part_2(data=BOXES) -> int:
-    return sum(sum(nsmallest(2, d)) * 2 + prod(d) for d in data)
+    return sum((sum(nsmallest(2, d)) << 1) + prod(d) for d in data)
 
 
 if __name__ == "__main__":
