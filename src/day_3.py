d001f96610b67a9314331ded920ddfbdb024fde8
Achxy <74144790+Achxy@users.noreply.github.com>
2022-08-15 20:33:01 +0530

    define constants in global scope

--- src/day_3.py
+++ src/day_3.py
@@ -4,6 +4,10 @@ from benchmark import advent_problem
 from data import day_3 as DATA
 
 
+SANTA_PATH = slice(None, None, 2)
+ROBO_SANTA_PATH = slice(1, None, 2)
+
+
 def _conv(i):
     gen = (complex(m < 0x3F and (-bool(m & 0x2) or 0x1), m > 0x5D and (-bool(m & 0x21) or 0x1)) for m in map(ord, i))
     return set(accumulate(gen, initial=0))
@@ -16,7 +20,8 @@ def part_1(data=DATA):
 
 @advent_problem
 def part_2(data=DATA):
-    return len(_conv(data[::2]) | _conv(data[1::2]))
+    santa, robot = data[SANTA_PATH], data[ROBO_SANTA_PATH]
+    return len(_conv(santa) | _conv(robot))
 
 
 if __name__ == "__main__":
