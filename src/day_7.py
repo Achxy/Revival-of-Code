e6a32a177649ec53e175f8d97eea0717efa87a5e
Achxy <74144790+Achxy@users.noreply.github.com>
2022-08-18 15:11:58 +0530

    add concise type annotation

--- src/day_7.py
+++ src/day_7.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
-from collections.abc import Callable
+from collections.abc import Callable, Iterable
 from benchmark import advent_problem
+from ast import dump
 from data import day_7 as DATA
 from functools import cache
 from typing import NamedTuple, TypeAlias
 from operator import and_, or_, lshift, rshift
 
-Scalar = int
-Wire = str
+Scalar: TypeAlias = int
+Wire: TypeAlias = str
+Instruction: TypeAlias = str
+Expression = str
 Node: TypeAlias = "BinOp | UnaryOp | Scalar | Wire"
 Operator = Callable[[Scalar, Scalar], Scalar]
 INSTRUCTION_MAP = {"AND": and_, "OR": or_, "LSHIFT": lshift, "RSHIFT": rshift, "NOT": lambda x: ~x & 0xFFFF}
@@ -25,14 +28,14 @@ class UnaryOp(NamedTuple):
 
 class Circuit:
     def __init__(self) -> None:
-        self._connections: dict = {}
+        self._connections: dict[Wire, Node] = {}
 
-    def take_instruction(self, instruction: str):
+    def take_instruction(self, instruction: Instruction):
         expr, target = map(str.strip, instruction.split("->"))
         self.set_wire(target, expr)
 
-    def set_wire(self, target: str, value: str):
-        self._connections[target] = self._form_connection(value)
+    def set_wire(self, target: Wire, expression: Expression):
+        self._connections[target] = self._form_connection(expression)
 
     def get_wire(self, wire: Wire):
         found = self._connections[wire]
@@ -42,14 +45,14 @@ class Circuit:
         self._unparse_tree.cache_clear()
 
     @classmethod
-    def from_instructions(cls, instructions: list[str]) -> Circuit:
+    def from_instructions(cls, instructions: Iterable[Instruction]) -> Circuit:
         self = cls()
         for instruction in instructions:
             self.take_instruction(instruction)
         return self
 
     @cache
-    def _unparse_tree(self, tree: Node):
+    def _unparse_tree(self, tree: Node) -> Scalar:
         if isinstance(tree, Scalar):
             return tree
         if isinstance(tree, Wire):
@@ -64,7 +67,7 @@ class Circuit:
             left, right = map(self._unparse_tree, [_left, _right])
             return op(left, right)
 
-    def _form_connection(self, expression: str):
+    def _form_connection(self, expression: Expression) -> Node:
         expr = expression.split()
         if len(expr) > 2:
             _left, _op, _right = expr
@@ -78,14 +81,10 @@ class Circuit:
         lone = expr.pop()
         return Scalar(lone) if lone.isnumeric() else lone
 
-    def _evaluate(self, value: str):
+    def _evaluate(self, value: Expression) -> Node:
         if value.isnumeric():
             return Scalar(value)
         return self._connections.get(value, value)
-
-    @property
-    def connections(self):
-        return self._connections.items()
 
 
 @advent_problem
