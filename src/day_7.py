0f190142512ca5939c18ee42893a283f550768fa
Achxy <74144790+Achxy@users.noreply.github.com>
2022-08-22 23:33:50 +0530

    actual fix

--- src/day_7.py
+++ src/day_7.py
@@ -2,17 +2,18 @@
 from __future__ import annotations
 
 from collections.abc import Callable, Iterable
-from functools import cache
+from functools import cache as memoize
 from operator import and_ as bin_and, lshift, or_ as bin_or, rshift
 from typing import TypeAlias, Union
-from ast import BinOp, UnaryOp
+from ast import AST
+from typing_extensions import reveal_type
 
 from benchmark import advent_problem
 from data import day_7 as DATA
 
 Scalar = int
 Wire = Instruction = Expression = str
-Node: TypeAlias = Union["Scalar", "Wire", "BinOp", "UnaryOp"]
+Node: TypeAlias = Union["Scalar", "Wire", "BinOp", "InvertOp"]
 INSTRUCTION_MAP = {
     "AND": bin_and,
     "OR": bin_or,
@@ -20,6 +21,28 @@ INSTRUCTION_MAP = {
     "RSHIFT": rshift,
     "NOT": lambda x: ~x & 0xFFFF,
 }
+
+
+def _evaluate(obj):
+    if isinstance(obj, str):
+        return Scalar(obj) if obj.isnumeric() else obj
+    return obj
+
+
+class BinOp(AST):
+    __slots__ = ("left", "op", "right")
+
+    def __init__(self, left: Node, op: str, right: Node) -> None:
+        self.left = _evaluate(left)
+        self.op: Callable = INSTRUCTION_MAP[op]
+        self.right = _evaluate(right)
+
+
+class InvertOp(AST):
+    __slots__ = ("operand",)
+
+    def __init__(self, operand: Node) -> None:
+        self.operand = _evaluate(operand)
 
 
 class Circuit:
@@ -47,7 +70,7 @@ class Circuit:
             target (Wire): Wire to which the the expression should be binded.
             expression (Expression): Expression which produces an scalar result
         """
-        self._connections[target] = self._form_connection(expression)
+        self._connections[target] = self._form_node(expression)
 
     def get_wire(self, wire: Wire) -> Scalar:
         """
@@ -61,13 +84,24 @@ class Circuit:
             Scalar: The scalar product which was an evaluated result of the tree parsing
         """
         tree = self.get_node(wire)
-        return self._unparse_tree(tree)
+        return self._unparse(tree)
 
     def get_node(self, name: str) -> Node:
-        return self._connections[name]
+        """
+        Takes an name and returns the node associated with the name stored in the internal connections
+        pool, chained names are cleared before returning, as such it can be ensured that returned name is not
+        a wire itself
 
-    def clear_cache(self):
-        self._unparse_tree.cache_clear()
+        Args:
+            name (str): The name to search
+
+        Returns:
+            Node: The node associated with the name
+        """
+        node = self._connections[name]
+        if isinstance(node, str):
+            return Scalar(node) if node.isnumeric() else self.get_node(node)
+        return node
 
     @classmethod
     def from_instructions(cls, instructions: Iterable[Instruction]) -> Circuit:
@@ -76,40 +110,30 @@ class Circuit:
             self.take_instruction(instruction)
         return self
 
-    @cache
-    def _unparse_tree(self, tree: Node) -> Scalar:
+    @memoize
+    def _unparse(self, tree: Node) -> Scalar:
         if isinstance(tree, Scalar):
             return tree
         if isinstance(tree, Wire):
-            ptr = self.get_node(tree)
-            return self._unparse_tree(ptr)
-        if isinstance(tree, InvertOp):
-            op = INSTRUCTION_MAP["NOT"]
-            value = self._unparse_tree(tree.operand)
-            return op(value)
+            node = self.get_node(tree)
+            return self._unparse(node)
         if isinstance(tree, BinOp):
-            _left, op, _right = tree.left, tree.op, tree.right
-            left, right = map(self._unparse_tree, [_left, _right])
-            return op(left, right)
+            left, right = map(self._unparse, [tree.left, tree.right])
+            return tree.op(left, right)
+        if isinstance(tree, InvertOp):
+            operand = self._unparse(tree.operand)
+            return INSTRUCTION_MAP["NOT"](operand)
+        raise ValueError(f"Expected node, found {tree!r}")
 
-    def _form_connection(self, expression: Expression) -> Node:
+    def _form_node(self, expression: Expression) -> Node:
         expr = expression.split()
         if len(expr) > 2:
-            _left, _op, _right = expr
-            left, right = map(self._evaluate, [_left, _right])
-            op = INSTRUCTION_MAP[_op]
-            return BinOp(left=left, op=op, right=right)
+            return BinOp(*expr)
         if len(expr) > 1:
-            _, _operand = expr
-            operand = self._evaluate(_operand)
+            _, operand = expr
             return InvertOp(operand=operand)
         lone = expr.pop()
-        return Scalar(lone) if lone.isnumeric() else lone
-
-    def _evaluate(self, value: Expression) -> Node:
-        if value.isnumeric():
-            return Scalar(value)
-        return self._connections.get(value, value)
+        return _evaluate(lone)
 
 
 @advent_problem
@@ -119,13 +143,9 @@ def part_1(data=DATA):
 
 @advent_problem
 def part_2(data=DATA):
-    # FIXME: ???
-    # Cache seems to get invalidated properly but we are getting the same values
-    # for both parts
     circuit = Circuit.from_instructions(data.splitlines())
-    # circuit.clear_cache()
-    circuit.set_wire("b", "3176")
-    circuit.clear_cache()
+    circuit.set_wire("b", str(circuit.get_wire("a")))
+    circuit._unparse.cache_clear()
     return circuit.get_wire("a")
 
 
